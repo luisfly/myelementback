7cb8f7a888804aa418038b5d4d922fb0db196efe
Louis <askeded@163.com>
2021-07-12 17:38:43 +0800

    7.7 上传

--- src/main/java/com/netback/myelementback/Dao/VideoMapper.java
+++ src/main/java/com/netback/myelementback/Dao/VideoMapper.java
@@ -1,6 +1,6 @@
 package com.netback.myelementback.Dao;
 
-import com.netback.myelementback.Entity.Resource;
+import com.netback.myelementback.Entity.Video;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -8,22 +8,22 @@ public interface VideoMapper {
 
     /**
      * 新建资源
-     * @param resource 资源信息
+     * @param video 资源信息
      */
-    public void insert(Resource resource);
+    public void insert(Video video);
 
     /**
      * 根据 ID 获取资源
      * @param nId
      * @return
      */
-    public Resource getById(int nId);
+    public Video getById(int nId);
 
     /**
      * 更新 Resource
-     * @param resource 资源的相关信息
+     * @param video 资源的相关信息
      */
-    public void update(Resource resource);
+    public void update(Video video);
 
     /**
      * 根据 id 删除 Resource
--- src/main/java/com/netback/myelementback/backcontrollers/VideoController.java
+++ src/main/java/com/netback/myelementback/backcontrollers/VideoController.java
@@ -2,6 +2,7 @@ package com.netback.myelementback.backcontrollers;
 
 import com.netback.myelementback.Dao.VideoMapper;
 import com.netback.myelementback.Entity.Resource;
+import com.netback.myelementback.Entity.Video;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,8 +22,53 @@ public class VideoController {
     @Autowired
     private VideoMapper mapper;
 
-    @PostMapping("/api/RadioUpload")
+    @PostMapping("/api/VideoUpload")
     public boolean UploadVideo(@RequestBody JSONObject postData) {
+        String videoName = postData.getAsString("VideoName");
+        String videoPath = postData.getAsString("VideoPath");
+        String userNO = postData.getAsString("UserNO");
+        String createUser = postData.getAsString("CreateUser");
+
+
+        Video video = new Video();
+        video.setsVideoName(videoName);
+        video.setsVideoPath(videoPath);
+        video.setsUserNO(userNO);
+        video.setsCreateUser(createUser);
+
+        SimpleDateFormat change = new SimpleDateFormat("YYYY-MM-dd");
+
+        try {
+            Date createDate = change.parse(postData.getAsString("CreateDate"));
+            video.setdCreateDate(createDate);
+            mapper.insert(video);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    @PostMapping("/api/VideoDel")
+    public String DelVideo(@RequestBody JSONObject postData) {
+        Number videoId = postData.getAsNumber("VideoId");
+
+        mapper.deleteById(videoId.intValue());
+
+        return "";
+    }
+
+    @PostMapping("/api/GetVideo")
+    public Video GetVideo(@RequestBody JSONObject postData) {
+        Number videoId = postData.getAsNumber("videoId");
+
+        Video video = mapper.getById(videoId.intValue());
+
+        return video;
+    }
+
+    @PostMapping("/api/VideoUpdate")
+    public boolean UpdateVideo(@RequestBody JSONObject postData) {
         String videoName = postData.getAsString("videoName");
         String videoPath = postData.getAsString("videoPath");
         String userNO = postData.getAsString("userNO");
@@ -39,12 +85,4 @@ public class VideoController {
         return true;
     }
 
-    @PostMapping("/api/RadioDel")
-    public String DelVideo(@RequestBody JSONObject postData) {
-        Number videoId = postData.getAsNumber("videoId");
-
-        mapper.deleteById(videoId.intValue());
-
-        return "";
-    }
 }
